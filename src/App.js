5093f60cd16ebb56fd27d01603e0cbb505da0206
Kunal77689 <83248197+Kunal77689@users.noreply.github.com>
2022-11-03 14:25:52 -0230

    Dark mode

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import Textform from "./Components/Textform";
 import Navbar from "./Components/Navbar";
+import About from "./Components/About";
 function App() {
   return (
     <div>
       <Navbar title="TextUtils" />
-      <Textform heading="Enter below to analyze" />
+      {/*<Textform heading="Enter below to analyze" />
+       */}
+      <About />
     </div>
   );
 }
